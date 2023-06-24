afb16ab3a562e129be0ab20868669010a79dfa05
akyc <akyc@users.noreply.github.com>
2023-06-24 14:57:14 +0300

    add ptoptypes

---
+++ src/components/App/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import AppHeader from '../AppHeader/AppHeader';
+import BurgerIngredients from '../BurgerIngredients/BurgerIngredients';
+import BurgerConstructor from '../BurgerConstructor/BurgerConstructor';
+
+import getData from '../../utils/data';
+
+import styles from './App.module.css';
+
+const App = () => {
+  const ingredientGroups = [
+    { name: "bun", title: "Булки" },
+    { name: "sauce", title: "Соусы" },
+    { name: "main", title: "Начинки" }
+  ]
+  const ingredients = getData()
+  return (
+    <div className="App">
+      <AppHeader />
+      <main className={styles.main}>
+        <BurgerIngredients ingredients={ingredients} ingredientGroups={ingredientGroups} />
+        <BurgerConstructor selectedIngredients={ingredients} />
+      </main>
+    </div>
+  );
+}
+
+export default App;
--- src/components/IngredientsList/IngredientsList.js
+++ src/components/IngredientsList/IngredientsList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from 'prop-types';
 import { Counter, CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 
 const IngredientsList = ({ ingredients, groupTitle }) => {
@@ -6,9 +7,9 @@ const IngredientsList = ({ ingredients, groupTitle }) => {
         <div>
             <p className="text text_type_main-default">{groupTitle}</p>
             <div className="pt-6 pb-10 pl-4" style={{ display: "flex", gap: '32px 24px', flexWrap: 'wrap', flexDirection: 'row', }}>
-                {ingredients.map(({ name, price, image_mobile, image_large, image }, i) => {
+                {ingredients.map(({ _id, name, price, image_mobile, image_large, image }) => {
                     return (
-                        <div key={i} style={{ position: 'relative', width: 272, flex: "0 0 auto" }} className="">
+                        <div key={_id} style={{ position: 'relative', width: 272, flex: "0 0 auto" }} className="">
                             <picture className="pl-4 pr-4 pb-4">
                                 <source media="(min-width: 1024px)" src={image_large} />
                                 <source media="(max-width: 768px)" src={image_mobile} />
@@ -20,13 +21,25 @@ const IngredientsList = ({ ingredients, groupTitle }) => {
                             <p className="text text_type_main-default" style={{ textAlign: 'center' }}>
                                 {name}
                             </p>
-                            {i === 0 && <Counter count={1} size="default" extraClass="m-1" />}
+                            {_id === 0 && <Counter count={1} size="default" extraClass="m-1" />}
                         </div>
                     )
                 })}
             </div>
         </div>
     )
+}
+
+IngredientsList.propTypes = {
+    ingredients: PropTypes.arrayOf(PropTypes.shape({
+        _id: PropTypes.string,
+        name: PropTypes.string,
+        price: PropTypes.number,
+        image_mobile: PropTypes.string,
+        image_large: PropTypes.string,
+        image: PropTypes.string
+    })).isRequired,
+    groupTitle: PropTypes.string.isRequired
 }
 
 export default IngredientsList
